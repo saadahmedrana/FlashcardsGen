cbf47a2e30272a6944cadc9437fc9226a3e8dd61
Saad Ahmed <sadisfaction570@gmail.com>
2024-08-18 14:29:38 +0500

    akal

--- firebase.js
+++ firebase.js
@@ -1,15 +1,29 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCz2kKiEnCGeEO2jP9zi0csa-aevISwJPA",
-    authDomain: "flashcard-saas-ec404.firebaseapp.com",
-    projectId: "flashcard-saas-ec404",
-    storageBucket: "flashcard-saas-ec404.appspot.com",
-    messagingSenderId: "584732940291",
-    appId: "1:584732940291:web:f571aa2158443b54f55636",
-    measurementId: "G-D9D8LTWD73"
+  apiKey: "AIzaSyAdIzZUrgOAU8ntnurwpmLLxYr4Q8QD6b4",
+  authDomain: "flashcardsgen.firebaseapp.com",
+  projectId: "flashcardsgen",
+  storageBucket: "flashcardsgen.appspot.com",
+  messagingSenderId: "487564457288",
+  appId: "1:487564457288:web:e7200b68d9ada1a423ef7d",
+  measurementId: "G-YX512T2364"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export default db;
+
+// Initialize Analytics only in browser
+let analytics;
+if (typeof window !== 'undefined') {
+  isSupported().then(supported => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { db, analytics };
